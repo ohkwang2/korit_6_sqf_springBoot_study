7de65319b4440c1b7879bd70a642eade4578312f
ohkwang2 <dhrhkd007@naver.com>
2024-07-18 12:02:14 +0900

    springboot first commit

---
+++ src/main/java/com/study/rest/controller/DIAndIoC.java
@@ -0,0 +1,11 @@
+package com.study.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class DIAndIoC {
+    
+    @GetMapping("/di")
+    public Object di() {
+        return null;
+    }
+}
